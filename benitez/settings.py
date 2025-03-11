c035c928bb4ef62e5a49daa4c6989febe98ae653
root <root@ip-172-31-20-56.us-east-2.compute.internal>
2025-03-11 19:05:36 +0000

    Resolviendo conflictos y evitando sobrescribir db.sqlite3

--- benitez/settings.py
+++ benitez/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9*%w$ow!)u#3$rg+(w=o3sz6kf_p(e@dbted^*1l9lhecb$swi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- benitez/wsgi.py
+++ benitez/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'benitez.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'benitez.settings')
 
 application = get_wsgi_application()
